91482b4236e9ff715d5089b46036f54f00ffe8ec
황원태 <amlk31255@gmail.com>
2022-05-30 01:40:00 +0900

    sample

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { default: plugin } = require("tailwindcss/plugin");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// tailwind.config.js
